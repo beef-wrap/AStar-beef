02eb346e19b451ed4c991fab0dadfc11b5574339
Faris Mustafa <faris.mustafa@procore.com>
2025-03-21 16:43:26 -0700

    only use toolset for msvc

--- build.ts
+++ build.ts
@@ -4,38 +4,43 @@ import { $ } from "bun";
 
 $.nothrow();
 
-const PLATFORMS = { win32: 'windows', linux: 'linux', darwin: 'macos' }
+const PLATFORMS = { win32: "windows", linux: "linux", darwin: "macos" };
 const PLATFORM = PLATFORMS[os.platform()];
 const ARCH = os.arch();
 const DEBUG = "debug";
 const RELEASE = "release";
+const TOOLSET = PLATFORM === "windows" ? "-T ClangCL" : "";
 
 const prepare = async () => {
-    for (const config of [DEBUG, RELEASE]) {
-        await mkdir(`libs/${config}/${PLATFORM}_${ARCH}`, { recursive: true });
-    }
-}
+  for (const config of [DEBUG, RELEASE]) {
+    await mkdir(`libs/${config}/${PLATFORM}_${ARCH}`, { recursive: true });
+  }
+};
 
-const buildDebug = async () => await $`
-    cmake -S . -B AStar/build -DCMAKE_BUILD_TYPE=Debug -T ClangCl
+const buildDebug = async () =>
+  await $`
+    cmake -S . -B AStar/build -DCMAKE_BUILD_TYPE=Debug ${TOOLSET}
     cmake --build AStar/build --config Debug
-`
+`;
 
-const copyDebug = async () => await $`
+const copyDebug = async () =>
+  await $`
     cp astar/build/Debug/astar.lib libs/${DEBUG}/${PLATFORM}_${ARCH}
     cp astar/build/Debug/astar.pdb libs/${DEBUG}/${PLATFORM}_${ARCH}
     cp astar/build/astar.a libs/${DEBUG}/${PLATFORM}_${ARCH}
-`
+`;
 
-const buildRelease = async () => await $`
+const buildRelease = async () =>
+  await $`
     cmake -S . -B AStar/build -DCMAKE_BUILD_TYPE=Release 
     cmake --build AStar/build --config Release
-`
+`;
 
-const copyRelease = async () => await $`
+const copyRelease = async () =>
+  await $`
     cp AStar/build/Release/astar.lib libs/${RELEASE}/${PLATFORM}_${ARCH}
     cp AStar/build/astar.a libs/${RELEASE}/${PLATFORM}_${ARCH}
-`
+`;
 
 await prepare();
 await buildDebug();
